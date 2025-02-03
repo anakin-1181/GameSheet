30f1b6faa5bd665e18bf26736c9e6874eab78c19
Anakin Lai <anakin252lai@gmail.com>
2025-02-03 22:28:32 +0000

    fetch data from google sheet

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const config= {
+    API_KEY:"AIzaSyAP8XiDKpddoWglaALIIVHe9weV7xauVvE",
+    internalARsheetID:"1xRNrchxKEYEoeRGNYiC9t0IW24mKpFg-FrdfpylHYSU",
+    discoveryDocs: 'https://sheets.googleapis.com/$discovery/rest?version=v4'
+}
