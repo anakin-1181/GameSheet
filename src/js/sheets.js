c34f8f165d7a3ab46d8b0889cdc2cd06ecc9e16a
anakin-1181 <anakin-1181@users.noreply.github.com>
2025-02-09 00:40:01 +0000

    deploy: 97c6593cec88deb584505f23767c1f8c10aa279d

--- src/js/sheets.js
+++ src/js/sheets.js
@@ -26,6 +26,8 @@ class SheetsAPI {
                 discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
             });
 
+            console.log("From sheet.js, API_KEY: ", API_KEY, "SHEET_ID: ", SHEET_ID)
+
             this.isInitialized = true;
         } catch (error) {
             console.error('Error initializing Google Sheets API:', error);
