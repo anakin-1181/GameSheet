b23d52db90e774846510854d7fb4245d93951018
Anakin Lai <anakin252lai@gmail.com>
2025-02-09 00:33:53 +0000

    vercel api update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Score Visualiser
+# GameSheet
 1. Used google sheet API to fetch data
 2. Run logic in javascript
 3. Deploy as website using github page
--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,19 @@
-export const config= {
-    API_KEY: process.env.GSHEET_API_KEY,
-    SHEET_ID: process.env.GSHEET_ID
+function fetchData() {
+    return fetch('/api/getData')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response error');
+            }
+            return response.json();
+        })
+        .then(config => {
+            console.log('Received data:', config);
+            console.log("Data fetched")
+            return config;
+        })
+}
+
+export const config = {
+    API_KEY: fetchData().API_KEY,
+    SHEET_ID: fetchData().SHEET_ID
 }
