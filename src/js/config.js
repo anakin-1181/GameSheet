595d0faa72647c4c868d82ce24bffe4948b4d874
anakin-1181 <anakin-1181@users.noreply.github.com>
2025-02-09 00:49:16 +0000

    deploy: 4c290ed53768a4d09668dcba4f8639df743c6cc5

--- src/js/config.js
+++ src/js/config.js
@@ -7,16 +7,11 @@ function fetchData() {
             return response.json();
         })
         .then(config => {
-            console.log("from cjs, config: ", config)
-            console.log("from cjs, config.API_KEY: ", config.API_KEY)
-            console.log("from cjs, config: ", config.SHEET_ID)
             return config;
         })
 }
 
-const data = fetchData();
-
 export const config = {
-    API_KEY: data.API_KEY,
-    SHEET_ID: data.SHEET_ID
+    API_KEY: await fetchData().API_KEY,
+    SHEET_ID: await fetchData().SHEET_ID
 }
