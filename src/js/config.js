e92dd3745956aae7228d76058bdd13bd81418c86
Anakin Lai <anakin252lai@gmail.com>
2025-02-09 01:02:39 +0000

    vercel api update final

--- src/js/config.js
+++ src/js/config.js
@@ -13,6 +13,6 @@ async function fetchData() {
 }
 
 export const config = {
-    API_KEY: await fetchData().API_KEY,
-    SHEET_ID: await fetchData().SHEET_ID
+    API_KEY: (await fetchData()).API_KEY,
+    SHEET_ID: (await fetchData()).SHEET_ID
 }
