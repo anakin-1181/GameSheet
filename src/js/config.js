cbf92d62647a99569e711e4d48626e97d5e46fa7
Anakin Lai <anakin252lai@gmail.com>
2025-02-09 00:53:56 +0000

    vercel api update final

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,15 @@
-function fetchData() {
-    return fetch('/api/getData')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response error');
-            }
-            return response.json();
-        })
-        .then(config => {
-            return config;
-        })
+async function fetchData() {
+    try {
+        const response = await fetch('/api/getData');
+        if (!response.ok) {
+            throw new Error('Network response error');
+        }
+        const data = await response.json();
+        console.log('Received data:', data);
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 export const config = {
